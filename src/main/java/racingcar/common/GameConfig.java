cdf57aa58f63176f4f94ab8d01feed0a608f4563
Lisc <qqwer153@gmail.com>
2022-04-21 03:20:43 +0900

    feat : 입력받는 문자열(자동차 이름) 예외처리

---
+++ src/main/java/racingcar/common/GameConfig.java
@@ -0,0 +1,14 @@
+package racingcar.common;
+
+public class GameConfig {
+	public static final int MAX_CAR_NAME_LENGTH = 5;
+	public static final int MIN_CAR_COUNT = 2;
+	public static final int CAR_BASE_POSITION = 0;
+	public static final int GO_CONDITION = 4;
+	public static final int MIN_MOVE_DISTANCE = 0;
+	public static final int MAX_MOVE_DISTANCE = 9;
+
+	public static final String CAR_NAME_SEPERATOR = ",";
+	public static final String MOVE_DISPLAY = "-";
+	public static final String STOP_DISPLAY = " ";
+}
---
+++ src/main/java/racingcar/utils/ValidationUtils.java
@@ -0,0 +1,56 @@
+package racingcar.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import racingcar.common.GameConfig;
+import racingcar.common.Message;
+
+public class ValidationUtils {
+    public static boolean carNameAllChk(String carNames) {
+        List<String> carList = splitStringToList(carNames, GameConfig.CAR_NAME_SEPERATOR);
+        carNameCountChk(carList);
+        carNameUniqueChk(carList);
+        return true;
+    }
+
+    public static boolean carNameCountChk(List<String> carList) {
+        if (carList.size() < GameConfig.MIN_CAR_COUNT) {
+            throw new IllegalArgumentException(Message.ERROR_MIN_CAR_COUNT);
+        }
+        return true;
+    }
+
+    public static boolean carNameUniqueChk(List<String> carList) {
+        HashSet<String> carSet = new HashSet<String>(carList);
+        if (carList.size() != carSet.size()) {
+            throw new IllegalArgumentException(Message.ERROR_CAR_NOT_UNIQUE);
+        }
+        return true;
+    }
+
+    public static boolean carNameEmptyChk(String carName) {
+        if (carName.trim().isEmpty()) {
+            throw new IllegalArgumentException(Message.ERROR_CAR_NAME_EMPTY);
+        }
+        return true;
+    }
+
+    public static boolean carNameLengthChk(String carName) {
+        if (carName.length() > GameConfig.MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(Message.ERROR_CAR_NAME_LENGTH);
+        }
+        return true;
+    }
+
+    public static List<String> splitStringToList(String carNames, String separator) {
+        List<String> carList = new ArrayList<String>();
+        for (String carName : carNames.split(separator)) {
+            carNameEmptyChk(carName);
+            carNameLengthChk(carName);
+            carList.add(carName);
+        }
+        return carList;
+    }
+}
