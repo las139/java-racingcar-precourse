df17ce8e5af5ea0d49d215017d8d29bb49f04281
Lisc <qqwer153@gmail.com>
2022-04-21 23:59:03 +0900

    refactor : GameConfig 변수명 변경

--- src/main/java/racingcar/common/GameConfig.java
+++ src/main/java/racingcar/common/GameConfig.java
@@ -4,11 +4,10 @@ public class GameConfig {
 	public static final int MAX_CAR_NAME_LENGTH = 5;
 	public static final int MIN_CAR_COUNT = 2;
 	public static final int CAR_BASE_POSITION = 0;
-	public static final int GO_CONDITION = 4;
-	public static final int MIN_MOVE_DISTANCE = 0;
-	public static final int MAX_MOVE_DISTANCE = 9;
+	public static final int MOVE_CONDITION_NO = 4;
+	public static final int MIN_RANDOM_NO = 0;
+	public static final int MAX_RANDOM_NO = 9;
 
 	public static final String CAR_NAME_SEPERATOR = ",";
 	public static final String MOVE_DISPLAY = "-";
-	public static final String STOP_DISPLAY = " ";
 }
