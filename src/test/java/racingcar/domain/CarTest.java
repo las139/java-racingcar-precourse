35fffb420d3e63aee751edf0956b187e10eaa3cd
Lisc <qqwer153@gmail.com>
2022-04-22 23:32:12 +0900

    test : CarTest 공통 부분 BeforeEach 처리

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import racingcar.common.GameConfig;
 
 public class CarTest {
+    Car car;
+
+    @BeforeEach
+    void init() {
+        car = new Car("pobi", GameConfig.BASE_CAR_POSITION);
+    }
+
     @Test
     @DisplayName("랜덤숫자_생성_범위_0_9_체크")
     @RepeatedTest(1000)
     void 랜덤숫자_생성_범위_0_9_체크() {
-        Car car = new Car("pobi", GameConfig.CAR_BASE_POSITION);
         int no = car.generateRandomNo(GameConfig.MIN_RANDOM_NO, GameConfig.MAX_RANDOM_NO);
         assertThat(no).isBetween(GameConfig.MIN_RANDOM_NO, GameConfig.MAX_RANDOM_NO);
     }
@@ -24,7 +31,6 @@ public class CarTest {
     @DisplayName("랜덤값_4_미만_자동차_이동_체크")
     @ValueSource(ints = { 0, 1, 2, 3 })
     void 랜덤값_4_미만_자동차_이동_체크(int no) {
-        Car car = new Car("pobi", GameConfig.CAR_BASE_POSITION);
         assertThat(car.isGo(no)).isFalse();
     }
 
@@ -32,7 +38,6 @@ public class CarTest {
     @DisplayName("랜덤값_4_이상_자동차_이동_체크")
     @ValueSource(ints = { 4, 5, 6, 7, 8, 9 })
     void 랜덤값_4_이상_자동차_이동_체크(int no) {
-        Car car = new Car("pobi", GameConfig.CAR_BASE_POSITION);
         assertThat(car.isGo(no)).isTrue();
     }
 }
